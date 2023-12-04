39605af65c8841cb47ba47e64454ea9dd79d99ce
juliansim1729 <jsim@unc.edu>
2023-12-04 15:53:06 -0500

    Changed deidentifying process to work better with more algs

--- scripts/make_deidentified_data.R
+++ scripts/make_deidentified_data.R
@@ -1,8 +1,26 @@
 # library calls
 library(readr)
 
-# creating deidentified data
 df <- read_csv("source_data/Dry_Bean_Dataset.csv")
-deidentified <- df[, -ncol(df)]
 
-write_csv(deidentified, "intermediate_data/deidentified_beans.csv")
+df$Class <- paste0(substr(df$Class, 1, 1), tolower(substr(df$Class, 2, 10)))
+labeled500 <- df[sample(which(df$Class == "Seker"), 500),]
+
+for (class in unique(df$Class)[2:7]) {
+  samp <- df[sample(which(df$Class == class), 500),]   
+  labeled500 <- rbind(labeled500, samp)
+}
+
+labeled500 <- labeled500 %>% dplyr::select("Area", "AspectRation", "Extent",
+                                           "Solidity", "Compactness", "Class")
+labeled500$Area <- scale(labeled500$Area)
+labeled500$AspectRation <- scale(labeled500$AspectRation)
+labeled500$Extent <- scale(labeled500$Extent)
+labeled500$Solidity <- scale(labeled500$Solidity)
+labeled500$Compactness <- scale(labeled500$Compactness)
+
+# creating deidentified500 data
+deidentified500 <- labeled500[, -ncol(labeled500)]
+
+write_csv(deidentified500, "intermediate_data/deidentified_beans.csv")
+write_csv(labeled500, "intermediate_data/labeled_beans.csv")
