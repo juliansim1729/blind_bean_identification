678156744c9310bf8ed0fa14dcbb96a470b1d2d2
juliansim1729 <jsim@unc.edu>
2023-12-04 15:50:25 -0500

    Cleaned graphics up, better labels

--- scripts/make_class_counts.R
+++ scripts/make_class_counts.R
@@ -1,11 +1,20 @@
 # library calls
 library(readr)
 library(ggplot2)
+library(bbplot)
 
 # reading in data
 df <- read_csv("source_data/Dry_Bean_Dataset.csv")
 
+df$Class <- paste0(substr(df$Class, 1, 1), tolower(substr(df$Class, 2, 10)))
+
 output <- ggplot(data = df, aes(x = Class)) +
-    stat_count(fill = "blanchedalmond", color = "firebrick")
+    stat_count(fill = "blanchedalmond", color = "firebrick") +
+    geom_text(stat='count', aes(label=..count..), vjust=-1) +
+    labs(title = "Counts by Bean Species") + 
+    xlab("Bean Class") +
+    ylab("Count") +
+    bbc_style() +
+    theme(axis.text.x=element_text(size=15))
 
 ggsave("figures/counts_by_class.png", output)
