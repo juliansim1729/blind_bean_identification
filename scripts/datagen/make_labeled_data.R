e848620e2146b749ef04e4b7491f075b58d559cd
Julian Sim <54722630+juliansim1729@users.noreply.github.com>
2023-12-14 03:56:52 -0500

    Updated dependencies

--- scripts/datagen/make_labeled_data.R
+++ scripts/datagen/make_labeled_data.R
@@ -13,18 +13,21 @@ for (class in unique(df$Class)[2:7]) {
 
 # select pertinent rows and adjust for capitalization/typos
 labeled500 <- dplyr::select(labeled500, "Area", "AspectRation", "Extent",
-                            "Solidity", "Compactness", "Class")
+                            "Solidity", "Compactness", "roundness", 
+                            "ShapeFactor4", "Class")
 
 labeled500$Class <- paste0(substr(labeled500$Class, 1, 1), 
                            tolower(substr(labeled500$Class, 2, 10)))
 colnames(labeled500)[2] <- "AspectRatio"
+colnames(labeled500)[6] <- "Roundness"
 
 # scale all numeric rows
-for (col_num in ncol(labeled500)) {
+for (col_num in 1:ncol(labeled500)) {
   col <- labeled500[,col_num]
-  if (is.numeric(col)) {
+  if (is.numeric(col[[1]])) {
     labeled500[,col_num] <- scale(col)[,1]
   }
 }
 
-write_csv(labeled500, "intermediate_data/labeled_beans.csv")
+write_csv(labeled500, "intermediate_data/labeled_beans_sf4.csv")
+
